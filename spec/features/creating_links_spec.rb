d0cdf4b6016ecdfe7c629e870a3702064db4b3c4
Joe Cowton <joecowton@gmail.com>
2017-11-14 15:01:24 +0000

    feature test added

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,10 @@
+feature "Creating Links" do
+  scenario 'form submit creates new link' do
+    visit '/links/new'
+    fill_in('Title', :with => 'Makers')
+    fill_in('URL', :with => 'https://www.makersacademy.com')
+    expect(page).to have_content "Title: Makers URL: https://www.makersacademy.com" 
+
+
+  end
+end
