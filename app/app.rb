13d23dd05da7adb851ea43f244f8e800dd3b37fb
majdeddine <majda-jambouri@hotmail.fr>
2017-11-14 16:33:04 +0000

    configure the rack environment

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,8 @@
+ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
 require_relative 'models/link.rb'
 
 class BookmarkManager < Sinatra::Base
-
   get '/links' do
     @links = Link.all
     erb :'links/index'
