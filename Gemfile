bf725b771db6a0daa5857795a048d68620b5da2f
Joe Cowton <joecowton@gmail.com>
2017-11-14 12:11:02 +0000

    update feature test

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://www.rubygems.org'
 ruby '2.4.1'
 
 gem 'capybara'
-gem 'data_mapper', '1.2.0'
+gem 'data_mapper' #, '1.2.0'
 gem 'dm-postgres-adapter'
 gem 'rspec'
 gem 'rubocop'
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,6 +1,14 @@
 feature 'BookmarkManager Homepage' do
+
   scenario 'should contain link' do
-    visit '/'
-    expect(page).to have_link('google', href: 'https://www.google.com')
+
+    Link.create(url: 'https://www.google.com', name: 'Google')
+    visit '/links'
+    expect(page).to have_link('Google', href: 'https://www.google.com')
+
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('Google')
   end
 end
