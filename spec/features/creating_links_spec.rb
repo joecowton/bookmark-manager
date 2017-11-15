971e4bdc6df6558973015d8f5ad89815f6851910
Joe Cowton <joecowton@gmail.com>
2017-11-15 11:13:37 +0000

    added tag feature test

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -7,3 +7,14 @@ feature "Creating Links" do
     expect(page).to have_content "Title: Makers URL: https://www.makersacademy.com"
   end
 end
+
+feature "Adding tags" do
+  scenario 'adds single tag to a link' do
+    visit '/links/new'
+    fill_in('Title', :with => 'Makers')
+    fill_in('URL', :with => 'https://www.makersacademy.com')
+    fill_in('Tag', :with => 'spirituality / cults ')
+    click_button 'Submit'
+    expect(page).to have_content "Title: Makers URL: https://www.makersacademy.com Tag: spirituality / cults "
+  end
+end
