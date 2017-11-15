002e5e2bf985107f5a3ca1d9cbd431c3dacdac61
Eustaquio122 <tiagofsousa@gmail.com>
2017-11-15 15:26:59 +0000

    Adds Bob

---
+++ spec/features/user_accounts_spec.rb
@@ -0,0 +1,16 @@
+feature "User area" do
+  scenario 'signs up and redirects to links page' do
+    sign_up('bob', 'bob@bob.bob', 'bubbles')
+    expect(page).to have_current_path('/links')
+  end
+
+  scenario 'links page shows welcome message with user email' do
+    sign_up('bob', 'bob@bob.bob', 'bubbles')
+    expect(page).to have_content("Welcome bob - bob@bob.bob")
+  end
+
+  scenario 'User count increases after new sign up' do
+    sign_up('bob', 'bob@bob.bob', 'bubbles')
+    expect(page).to have_content("1 user")
+  end
+end
